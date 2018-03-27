039ed2551280530271decbc553f774887d1e5987
GustavoMalta <gustavo.a.malta@gmail.com>
2018-03-27 17:14:17 -0300

    First Commit

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
 "# Ionic-Curso" 
-"# Ionic-Curso" 
---
+++ src/pages/home/home.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+
+@Component({
+  selector: 'page-home',
+  templateUrl: 'home.html'
+})
+export class HomePage {
+public tugle: any = {teste:true}
+
+  constructor(public navCtrl: NavController) {
+
+  }
+
+clica (val : boolean){
+}
+
+public variavel:number = 1040;
+}
