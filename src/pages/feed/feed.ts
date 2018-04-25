bb129290835eb81430adcd39d8ef8864cf101b03
GustavoMalta <gustavo.a.malta@gmail.com>
2018-04-25 17:19:22 -0300

    Loading

--- src/pages/feed/feed.ts
+++ src/pages/feed/feed.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { MovieProvider } from '../../providers/movie/movie';
-
+import { LoadingController } from 'ionic-angular';
 /**
  * Generated class for the FeedPage page.
  *
@@ -30,21 +30,36 @@ export class FeedPage {
   constructor(
   	public navCtrl: NavController, 
   	public navParams: NavParams,
-  	private movieProvider: MovieProvider
+  	private movieProvider: MovieProvider,
+  	public loadingCtrl: LoadingController
   	) {
   }
 
   public result: any;
+  public loader:any;
 
-  ionViewDidLoad() {
+iniciaLoad() {
+    this.loader = this.loadingCtrl.create({
+      content: "Please wait..."
+    });
+    this.loader.present();
+  }
+terminaLoad(){
+	this.loader.dismiss();
+}
+
+  ionViewDidEnter() { //Entrer para carregar os filmes sempre que entrar. oLoad carrega so uma vez
+    this.iniciaLoad()
     this.movieProvider.getPopularMovies().subscribe(
 								data => {
 									const response = (data as any);
 									const resposta = JSON.parse(response._body);
 									this.result=resposta.results;
 									console.log(resposta.results);
+									this.terminaLoad();
 									 }, error =>{
 									 	console.log(error);
+									 	this.terminaLoad();
 									 }
 								)
   }
