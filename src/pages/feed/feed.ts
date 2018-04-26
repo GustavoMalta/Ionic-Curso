71a39fe78ced798c794ec6a4306b1ae8bb89024f
GustavoMalta <gustavo.a.malta@gmail.com>
2018-04-26 10:10:27 -0300

    Loading Refresher

--- src/pages/feed/feed.ts
+++ src/pages/feed/feed.ts
@@ -9,61 +9,84 @@ import { LoadingController } from 'ionic-angular';
  * Ionic pages and navigation.
  */
 
-@IonicPage()
-@Component({
-  selector: 'page-feed',
-  templateUrl: 'feed.html',
-  providers: [ MovieProvider ],
-})
-export class FeedPage {
+ @IonicPage()
+ @Component({
+ 	selector: 'page-feed',
+ 	templateUrl: 'feed.html',
+ 	providers: [ MovieProvider ],
+ })
+ export class FeedPage {
 
-	public objeto_feed = {
-		titulo: 'Clint Estwood',
-		data: 'November 5, 1985',
-		descricao: 'Wait a minute. Wait a minute, Doc. Uhhh... Are you telling me that you built a time machine... out of a DeLorean?! Whoa. This is heavy.',
-		likes: 42,
-		coments: 99,
-		hora_coment: "13h Ago"
+ 	public objeto_feed = {
+ 		titulo: 'Clint Estwood',
+ 		data: 'November 5, 1985',
+ 		descricao: 'Wait a minute. Wait a minute, Doc. Uhhh... Are you telling me that you built a time machine... out of a DeLorean?! Whoa. This is heavy.',
+ 		likes: 42,
+ 		coments: 99,
+ 		hora_coment: "13h Ago"
 
-	}
+ 	}
 
-  constructor(
-  	public navCtrl: NavController, 
-  	public navParams: NavParams,
-  	private movieProvider: MovieProvider,
-  	public loadingCtrl: LoadingController
-  	) {
-  }
+ 	constructor(
+ 		public navCtrl: NavController, 
+ 		public navParams: NavParams,
+ 		private movieProvider: MovieProvider,
+ 		public loadingCtrl: LoadingController
+ 		) {
+ 	}
 
-  public result: any;
-  public loader:any;
+ 	public result: any;
+ 	public loader:any;
+ 	public refresher = null;
+ 	public isRefreching:boolean;
 
-iniciaLoad() {
-    this.loader = this.loadingCtrl.create({
-      content: "Please wait..."
-    });
-    this.loader.present();
-  }
-terminaLoad(){
-	this.loader.dismiss();
+ 	iniciaLoad() {
+ 		this.loader = this.loadingCtrl.create({
+ 			content: "Carregando Filmes..."
+ 		});
+ 		this.loader.present();
+ 	}
+ 	terminaLoad(){
+ 		this.loader.dismiss();
+ 	}
+
+ 	doRefresh(refresher) {
+ 		this.refresher=refresher;
+ 		this.carregaFilmes();
+
+ 	}
+
+  ionViewDidEnter() { //'Entrer' para carregar os filmes sempre que entrar. o'Load' carrega so uma vez
+  this.carregaFilmes();
 }
 
-  ionViewDidEnter() { //Entrer para carregar os filmes sempre que entrar. oLoad carrega so uma vez
-    this.iniciaLoad()
-    this.movieProvider.getPopularMovies().subscribe(
-								data => {
-									const response = (data as any);
-									const resposta = JSON.parse(response._body);
-									this.result=resposta.results;
-									console.log(resposta.results);
-									this.terminaLoad();
-									 }, error =>{
-									 	console.log(error);
-									 	this.terminaLoad();
-									 }
-								)
-  }
+carregaFilmes(){
+	this.iniciaLoad()
+
+	this.movieProvider.getPopularMovies().subscribe(
+		data => {
+			const response = (data as any);
+			const resposta = JSON.parse(response._body);
+			this.result=resposta.results;
+			console.log(resposta);
+			this.terminaLoad();
+			if(this.refresher != null){ //para nao dar o .compete antes de ter algum dado na variavel, sem usar outra variavel para verificação
+				this.refresher.complete();
+				this.refresher = null;
+			}
+		}, error =>{
+			console.log(error);
+			this.terminaLoad();
+			if(this.refresher != null){ 
+				this.refresher.complete();
+				this.refresher = null;
+			}
+		}
+		)
+
+	
 
 
+}
 
 }
