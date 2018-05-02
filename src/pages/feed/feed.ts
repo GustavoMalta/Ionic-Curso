ab9b43c8836244f7a95e3286750b786728c617d0
GustavoMalta <gustavo.a.malta@gmail.com>
2018-05-02 09:24:47 -0300

    Infinit Scroll

--- src/pages/feed/feed.ts
+++ src/pages/feed/feed.ts
@@ -40,6 +40,8 @@ import { FilmeDetalhesPage } from '../filme-detalhes/filme-detalhes';
  	public result: any;
  	public loader: any;
  	public refresher = null;
+ 	public page:number =1;
+ 	public infiniteScroll;
 
  	iniciaLoad() {
  		this.loader = this.loadingCtrl.create({
@@ -58,24 +60,42 @@ import { FilmeDetalhesPage } from '../filme-detalhes/filme-detalhes';
  	}
 
   ionViewDidEnter() { //'Entrer' para carregar os filmes sempre que entrar. o'Load' carrega so uma vez
+  this.page=1;
+  this.result=null;
   this.carregaFilmes();
 }
 
 carregaFilmes(){
-	this.iniciaLoad()
-
-	this.movieProvider.getPopularMovies().subscribe(
+	this.iniciaLoad();
+	this.movieProvider.getPopularMovies(this.page).subscribe(
 		data => {
 			const response = (data as any);
 			const resposta = JSON.parse(response._body);
-			this.result=resposta.results;
-			console.log(resposta);
+			//if(infinitPage){
+				
+				if (this.page>1){
+					this.result=this.result.concat(resposta.results);
+					this.infiniteScroll.complete();
+					this.terminaLoad();
+				}else{
+					this.result=resposta.results;
+					this.terminaLoad();
+					if(this.refresher != null){ //para nao dar o .compete antes de ter algum dado na variavel, sem usar outra variavel para verificação
+						this.refresher.complete();
+						this.refresher = null;
+					}
+				}
+		/*	}else{
+				this.result=resposta.results;
+				console.log("teste");
+			}
+			console.log(this.result);
 			this.terminaLoad();
 			if(this.refresher != null){ //para nao dar o .compete antes de ter algum dado na variavel, sem usar outra variavel para verificação
 				this.refresher.complete();
 				this.refresher = null;
-			}
-		}, error =>{
+			}*/
+		},error =>{
 			console.log(error);
 			this.terminaLoad();
 			if(this.refresher != null){ 
@@ -88,8 +108,13 @@ carregaFilmes(){
 
 
 detalhes(id){
-
 	this.navCtrl.push(FilmeDetalhesPage, id);
 }
 
+doInfinite(infiniteScroll) {
+	this.infiniteScroll = infiniteScroll;
+	this.page++;
+	console.log('Begin async operation'+ this.page);
+	this.carregaFilmes();
+}
 }
